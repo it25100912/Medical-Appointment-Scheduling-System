81ed2788379a3f78b3426de82c9e89f86e0f8356
IT25100912 <it25100912@my.Sliit.lk>
2026-05-19 00:37:36 +0530

    Updated User.java

--- Backend/src/main/java/com/medicalapp/auth/entity/User.java
+++ Backend/src/main/java/com/medicalapp/auth/entity/User.java
@@ -1,12 +1,15 @@
 package com.medicalapp.auth.entity;
 
+import jakarta.persistence.*;
+
 public class User {
 
+    @Column(unique = true)
     private String email;
     private String password;
     private String name;
     private String nic;
     private String phone;
     private String licenseNumber;
-    
+
 }
