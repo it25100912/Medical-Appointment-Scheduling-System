089f2b91425d995c4ad79f739953f3b8e94eb579
IT25100483 <it25100483@my.sliit.lk>
2026-05-19 01:21:03 +0530

    Create PatientDTO.java

---
+++ Backend/src/main/java/com/medicalapp/patient/dto/PatientDTO.java
@@ -0,0 +1,26 @@
+package com.medicalapp.patient.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class PatientDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "NIC is required")
+    private String nic;
+
+    @NotBlank(message = "Phone number is required")
+    private String phone;
+
+    @Email(message = "Invalid email format")
+    private String email;
+
+
+
+}
