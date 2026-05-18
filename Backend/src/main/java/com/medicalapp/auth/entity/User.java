444dd25bac407bcda8d20681defc78fb16c2b550
IT25100912 <it25100912@my.Sliit.lk>
2026-05-19 00:36:22 +0530

    Created User.java

---
+++ Backend/src/main/java/com/medicalapp/auth/entity/User.java
@@ -0,0 +1,12 @@
+package com.medicalapp.auth.entity;
+
+public class User {
+
+    private String email;
+    private String password;
+    private String name;
+    private String nic;
+    private String phone;
+    private String licenseNumber;
+    
+}
