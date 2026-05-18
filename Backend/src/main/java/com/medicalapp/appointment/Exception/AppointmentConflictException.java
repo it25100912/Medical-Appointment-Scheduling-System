e9e804c0b8bc87fb282f125b3595c6240c33adc6
it25103256 <it25103256@my.sliit.lk>
2026-05-18 14:00:57 +0530

    update IAppointment service

--- Backend/src/main/java/com/medicalapp/appointment/Exception/AppointmentConflictException.java
+++ Backend/src/main/java/com/medicalapp/appointment/Exception/AppointmentConflictException.java
@@ -1,4 +1,11 @@
 package com.medicalapp.appointment.Exception;
 
-public class AppointmentConflictException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class AppointmentConflictException extends RuntimeException {
+    public AppointmentConflictException(String message) {
+        super(message);
+    }
 }
