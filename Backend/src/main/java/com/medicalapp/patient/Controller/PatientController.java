9101e6502c7ac99badc97c45e6b507768f4d43fd
IT25100483 <it25100483@my.sliit.lk>
2026-05-19 01:23:33 +0530

    Patient API

--- Backend/src/main/java/com/medicalapp/patient/Controller/PatientController.java
+++ Backend/src/main/java/com/medicalapp/patient/Controller/PatientController.java
@@ -1,4 +1,12 @@
 package com.medicalapp.patient.Controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/patients")
 public class PatientController {
 }
